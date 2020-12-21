f9b8716dc30d27ba6f218d4302b0d55f254621b0
symehmoo <symehmoo@gmail.com>
2020-12-21 12:33:41 -0500

    added git Checkout

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,7 +1,7 @@
 def call(Map stageParams){
 checkout([
         $class: 'GitSCM',
-        branches: [[name: '*/master']],
+        branches: [[name: '*/main']],
         doGenerateSubmoduleConfigurations: false,
         extensions: [],
         submoduleCfg: [],

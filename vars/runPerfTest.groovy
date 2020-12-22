527e97909678024219fd6264c8854779130f806b
symehmoo <symehmoo@gmail.com>
2020-12-22 10:52:03 -0500

    echo msgs

--- vars/runPerfTest.groovy
+++ vars/runPerfTest.groovy
@@ -5,7 +5,7 @@ def call(Map params){
     def testType= params.get("testType", true)
     echo "works"
     echo "hello"
-   echo $testType
-    echo $testName
+   echo ${testType}
+    echo ${testName}
 
 }

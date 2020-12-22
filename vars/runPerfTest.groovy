3f009efd085484b2a30215afec2bd030a2d0c81e
symehmoo <symehmoo@gmail.com>
2020-12-22 11:20:08 -0500

    echo msgs an dprint

--- vars/runPerfTest.groovy
+++ vars/runPerfTest.groovy
@@ -3,10 +3,13 @@ import java.util.logging.Logger
 
 def call(Map params){
     def testName= params.get("testName")
-    def testType= params.getOrDefault("testType", true)
+    def testType= params.getOrDefault("testType", pipeline)
+    def userName = params.get("userName")
     echo "works"
     echo "hello"
     println testName
+    println testType
+    println userName
 
 
 }

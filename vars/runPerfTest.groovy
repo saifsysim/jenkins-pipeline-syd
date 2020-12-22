bc8b1384c02d771802852be90b554e878481f963
symehmoo <symehmoo@gmail.com>
2020-12-22 12:02:39 -0500

    added code for run test + corrected camel case for testName + rocess print + assertiuons

--- vars/runPerfTest.groovy
+++ vars/runPerfTest.groovy
@@ -12,6 +12,8 @@ def call(Map params){
     println userName
     println env
 
+    assert testName != null:"please provide Test name"
+    assert userName != null:"please provide User name"
     def sout = new StringBuilder(), serr = new StringBuilder();
     Logger logger = Logger.getLogger("testName")
     def process = [ 'bash', '-c', "curl -X POST -H \"Content-Type: application/json\"  -H \"Accept: */*\" -H \"Cache-Control: no-cache\" -H \"Postman-Token: f7f691b9-dcf9-6371-2b74-beb8517e3a5a0\" -d '{ \"repeatTest\": \"yes\",\"testName\": \"${testName}\", \"uesrs_userId\": \"${userName}\"}' http://gvez9.mocklab.io/run/runTest" ].execute()

62cfd78feeacca9f198c0d91ee4d39c23a927cc4
symehmoo <symehmoo@gmail.com>
2020-12-22 11:18:17 -0500

    echo msgs an dprint

--- vars/runPerfTest.groovy
+++ vars/runPerfTest.groovy
@@ -1,10 +1,12 @@
 import groovy.json.*
+import java.util.logging.Logger
 
 def call(Map params){
     def testName= params.get("testName")
     def testType= params.getOrDefault("testType", true)
     echo "works"
     echo "hello"
+    println testName
 
 
 }
